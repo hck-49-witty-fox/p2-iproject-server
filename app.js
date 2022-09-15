5b344168113f0eabd02545bc33b49b3eda616bad
Nabiel Alif <nabiel.alif01@gmail.com>
2022-09-15 10:24:31 +0700

    changing port to uppercase

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routes');
 const app = express();
 const cors = require('cors');
-const port = 3000 || process.env.port;
+const port = 3000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
